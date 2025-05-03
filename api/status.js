73fbb59a422f9417f5bcf77f0c556e229ea9670d
GraphStats <no.replybot5455@gmail.com>
2025-05-03 12:42:07 +0200

    Update status.js

--- api/status.js
+++ api/status.js
@@ -5,7 +5,7 @@ const PORT = 3000;
 
 const services = [
   { name: 'FastCount', url: 'http://fast-count.vercel.app/' },
-  { name: 'API Count', url: 'https://ests.sctools.org/api/get/UCX6OQ3DkcsbYNE6H8uQQuVA' },
+  { name: 'API Count', url: 'https://backend.mixerno.space/api/youtube/estv3/UCX6OQ3DkcsbYNE6H8uQQuVA' },
   { name: 'API Search', url: 'https://mixerno.space/api/youtube-channel-counter/search/mrbeast' },
   { name: 'FastCount Status', url: 'https://fast-count-status.vercel.app/' }
 ];
